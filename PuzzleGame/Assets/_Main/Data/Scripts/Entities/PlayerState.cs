fd4d3fa623a338aada6ae4bc3b65f68a7206ef83
Lu <102192546+Rucas02@users.noreply.github.com>
2022-05-09 19:41:20 -0300

    Grounded funciona con el Player

--- PuzzleGame/Assets/_Main/Data/Scripts/Entities/PlayerState.cs
+++ PuzzleGame/Assets/_Main/Data/Scripts/Entities/PlayerState.cs
@@ -11,12 +11,13 @@ public class PlayerState : MonoBehaviour
     //IS GROUND FUNCTION
     public bool IsGrounded()
     {
-        Ray ray = new Ray(this.transform.position + Vector3.down * 0.25f, Vector3.down);
-        Debug.Log(Vector3.down * 1);
-        if (Physics.Raycast(ray, out RaycastHit hit, 0.25f, floorLayerMask))
+        if(Physics.Raycast(transform.position, -transform.up, out RaycastHit hit, 0.25f))
             return true;
         else
+        {
             Debug.Log("no toca");
-        return false;
+            return false;
+        }
+            
     }
 }

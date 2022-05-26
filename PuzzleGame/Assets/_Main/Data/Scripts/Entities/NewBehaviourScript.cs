c23987ea154854fce737a465b2790a5ec91f0352
Rucas02 <campolucas2001@gmail.com>
2022-05-26 11:34:23 -0300

    El trampolin detecta si es la piramide, cubo o la esfera como lo hacia antes, pero ahora con el overlapbox

--- PuzzleGame/Assets/_Main/Data/Scripts/Entities/NewBehaviourScript.cs
+++ PuzzleGame/Assets/_Main/Data/Scripts/Entities/NewBehaviourScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    private float _currentForce;
     [SerializeField] private float force = 200f;
     [SerializeField] private LayerMask layerMask;
     
@@ -27,11 +28,29 @@ public class NewBehaviourScript : MonoBehaviour
         {
             foreach (Collider collition in hitColliders)
             {
-                collition.GetComponent<Rigidbody>().AddForce(transform.up * force * Time.deltaTime, ForceMode.Impulse);
+                Player player = collition.GetComponent<Player>();
+
+                if (player)
+                {
+                    if (player.Data.ID == "PSR")
+                        _currentForce = force * 2.5f;
+                    else if (player.Data.ID == "PPM")
+                        _currentForce = force / 15;
+                    else
+                        _currentForce = force / 2;
+                }
+                else
+                {
+                    _currentForce = force;
+                }
+                
+                
+                collition.GetComponent<Rigidbody>().AddForce(transform.up * _currentForce * Time.deltaTime, ForceMode.Impulse);
             }
             i++;
         }
     }
+
     
     void OnDrawGizmos()
     {

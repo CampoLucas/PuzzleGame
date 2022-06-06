bf6814b619462b70f760b3feda2298c13121c469
Dantetisso <dantetisoriero@gmail.com>
2022-06-06 15:30:57 -0300

    actualizacion prueba de inflable
    le meti una cajita e hice que se active la animacion donde se "infla".
    falta hacer que reaccione a los estados del player

---
+++ PuzzleGame/Assets/_Main/Data/Scripts/Props/Inflable.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inflable : MonoBehaviour
+{
+    [SerializeField] private bool IsPressed = false;
+    public bool prevState;
+
+    private Animator _anim;
+   
+    private Player player;
+
+    private void Awake()
+    {
+        _anim = GetComponent<Animator>();
+    } 
+
+    void Update()
+    {
+        _anim.SetBool("IsPressed", IsPressed);
+
+        if (IsPressed && prevState != IsPressed)
+            Inflate();
+        if (!IsPressed && prevState != IsPressed)
+            Deflate();
+
+    }
+
+    private void Inflate()
+    {
+        prevState = IsPressed;
+        
+    }
+
+    private void Deflate()
+    {
+        prevState = IsPressed;
+        
+    }
+
+    public void Setinflate(bool isOpen) => IsPressed = isOpen;
+
+
+
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {     
+        if (player)
+        {
+            if (player.Data.ID == "PSR")
+            {
+                _anim.SetBool("IsSphereOn", true);
+            }
+            else
+            {
+                _anim.SetBool("IsSphereOn", false);
+            }
+
+            if (player.Data.ID == "PCB" && other.gameObject.CompareTag("Player"))
+            {
+                _anim.SetBool("IsCubeOn", true);
+            }
+            else
+            {
+                _anim.SetBool("IsCubeOn", false);
+            }
+
+                       
+
+        }
+
+
+    }
+      
+}

a21a86b61e4e41cceb51f7497615d9dbb6888c51
Lu <102192546+Rucas02@users.noreply.github.com>
2022-06-12 08:17:01 -0300

    Le cambie los modelos del player para que usen el shade
    no se ven de lo mejor, pero va adquiriendo

--- PuzzleGame/Assets/_Main/Data/Scripts/Entities/Player/PlayerState.cs
+++ PuzzleGame/Assets/_Main/Data/Scripts/Entities/Player/PlayerState.cs
@@ -15,6 +15,7 @@ public class PlayerState : MonoBehaviour
     {
         var transform1 = transform;
         _isGrounded = Physics.Raycast(transform1.position, -transform1.up, out RaycastHit hit, 0.25f);
+        
     }
 
     public void SetIsInteracting(bool isInteracting) => _isInteracting = isInteracting;
